a6d19cd37d7b66ddabb908c18d3d110793905dc9
risvanthkm <risvanth129@gmail.com>
2026-07-29 20:10:13 +0530

    first commit

---
+++ 1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True
+)
+
+@app.route("/")
+def home():
+    r.set("message", "Hello Redis")
+    return r.get("message")
+
+app.run(host="0.0.0.0", port=8000)
---
+++ script.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+touch docker-compose.yaml
+echo services: > docker-compose.yaml
+
+declare -A bridges
+
+while true; do
+    echo "Input Required Variables Please"
+
+    IFS=',' read -r name base_image dep_file dir stup_cmd host_port cont_port bridge mount dep
+
+    if [[ -z "$name" || -z "$base_image" || -z "$dep_file" || -z "$dir" || -z "$stup_cmd" || -z "$host_port" || -z "$cont_port" || -z "$bridge" || -z "$mount" || -z "$dep" ]]; then
+        echo "Stopping Input : One or more required variables are empty."
+        break
+    fi
+
+    bridges["$bridge"]=$bridge
+
+    (
+        mkdir -p "$dir"
+        cd "$dir"
+        touch Dockerfile
+
+        cat <<- EOF > Dockerfile
+FROM $base_image
+WORKDIR /app
+COPY $dep_file ./
+RUN pip install -r $dep_file
+COPY . .
+RUN $stup_cmd
+
+EOF
+
+    nano Dockerfile
+
+    )
+
+    cat <<EOF >> docker-compose.yaml
+    $name:
+        build: $dir
+        ports:
+            - $host_port:$cont_port
+        networks:
+            - $bridge
+        restart: unless-stopped
+        volumes: 
+            - $mount
+        depends_on:
+            $dep :
+                condition: service_healthy
+EOF
+
+    nano docker-compose.yaml
+
+done
+
+echo networks: >> docker-compose.yaml
+
+for br in ${!bridges[@]}; do
+echo $br
+cat <<EOF >> docker-compose.yaml
+    $br:
+        driver: bridge
+EOF
+
+done
+
+docker compose up 
+
+docker compose logs
+
+#app,python:3.12-slim,requirments.txt,./1,python app.py,8081,8081,bridge1,./:/,redis
+#redis,redis:7-alpine,dep.txt,./2,echo,6379,6379,bridge1,./:/,redis
